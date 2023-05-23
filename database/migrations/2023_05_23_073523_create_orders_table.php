e3c217a56025722f8885cc3ea305b938a534faab
“Yomar” <team.yob10@gmail.com>
2023-05-23 03:46:12 -0400

    stable

---
+++ database/migrations/2023_05_23_073523_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id');
+            $table->foreignId('client_id');
+            $table->unsignedInteger('index');
+            $table->enum('status', ['paused', 'canceled', 'active', 'finished'])->default('active');
+            $table->datetime('finished_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
