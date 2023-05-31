c87fe4ddd0fbdc324860ad5621ead84a71a0568b
“Yomar” <team.yob10@gmail.com>
2023-05-31 04:48:00 -0400

    stable

--- resources/js/Pages/Admin/CreateOrder.vue
+++ resources/js/Pages/Admin/CreateOrder.vue
@@ -16,53 +16,58 @@
                         :name="1"
                         title="Información básica"
                         icon="edit">
-                        <q-select
-                            label="Seleccionar comercio"
-                            stack-label
-                            :map-options="true"
-                            :emit-value="true"
-                            :options="admins"
-                            option-value="id"
-                            option-label="name"
-                            v-model="form.user_id"
-                            :error="Boolean(form.errors.user_id)"
-                            :error-message="form.errors.user_id">
-                        </q-select>
+                        <form ref="firstStepForm">
+                            <q-select
+                                label="Seleccionar comercio"
+                                stack-label
+                                :map-options="true"
+                                :emit-value="true"
+                                :options="admins"
+                                option-value="id"
+                                option-label="name"
+                                v-model="form.user_id"
+                                :error="Boolean(form.errors.user_id)"
+                                :error-message="form.errors.user_id">
+                            </q-select>
 
-                        <q-input
-                            stack-label
-                            label="Cédula"
-                            v-model="form.dni"
-                            :error="Boolean(form.errors.dni)"
-                            :error-message="form.errors.dni">
-                        </q-input>
+                            <q-input
+                                stack-label
+                                label="Cédula"
+                                v-model="form.dni"
+                                :error="Boolean(form.errors.dni)"
+                                :error-message="form.errors.dni"
+                                required>
+                            </q-input>
 
-                        <q-input
-                            stack-label
-                            label="Nombre"
-                            v-model="form.name"
-                            :error="Boolean(form.errors.name)"
-                            :error-message="form.errors.name">
-                        </q-input>
+                            <q-input
+                                stack-label
+                                label="Nombre"
+                                v-model="form.name"
+                                :error="Boolean(form.errors.name)"
+                                :error-message="form.errors.name"
+                                required>
+                            </q-input>
 
-                        <q-input
-                            stack-label
-                            label="Teléfono"
-                            v-model="form.phone"
-                            :error="Boolean(form.errors.phone)"
-                            :error-message="form.errors.phone">
-                        </q-input>
+                            <q-input
+                                stack-label
+                                label="Teléfono"
+                                v-model="form.phone"
+                                :error="Boolean(form.errors.phone)"
+                                :error-message="form.errors.phone"
+                                required>
+                            </q-input>
+                        </form>
                     </q-step>
                     <q-step :name="2" title="Descripción" :done="step > 3" icon="description">
-                        <div class="q-gutter-y-md">
-                        <template v-for="formField in formFields">
+                        <form ref="secondStepForm" class="q-gutter-y-md">
+                            <template v-for="formField in formFields">
                                 <q-input stack-label
                                     v-model="form.info[formField.id]"
                                     :label="formField.label"
                                     :required="formField.required">
                                 </q-input>
-                        </template>
-                        </div>
+                            </template>
+                        </form>
                     </q-step>
                     <q-step :name="3" title="Seleccionar servicios" :done="step > 2" icon="shopping_bag">
                         <q-option-group
@@ -75,8 +80,7 @@
                     <template v-slot:navigation>
                         <q-stepper-navigation class="flex justify-end">
                             <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Volver" class="q-ml-sm" />
-                            <q-btn v-if="step != 3" @click="$refs.stepper.next()" color="primary" :label="step === 3 ? 'Terminar' : 'Continuar'" />
-                            <q-btn v-else @click="submit" color="primary" label="Guardar" :loading="form.processing"/>
+                            <q-btn v-if="step <= 4" @click="skipFormStep($refs.stepper)" color="primary" :label="'Continuar'" :loading="form.processing"/>
                         </q-stepper-navigation>
                     </template>
                 </q-stepper>
@@ -90,8 +94,11 @@ import { computed, ref, watch } from 'vue';
 import { useQuasar } from 'quasar';
 
     const $q = useQuasar()
+    const stepper = ref(null)
     const step = ref(1)
     const props = defineProps({admins: Array})
+    const firstStepForm = ref(null)
+    const secondStepForm = ref(null)
 
     const form = useForm({
         user_id: null,
@@ -101,6 +108,28 @@ import { useQuasar } from 'quasar';
         services: [],
         info: {}
     })
+
+    function skipFormStep(stepper){
+        if( step.value == 1 ){
+            form.errors.user_id = null
+            if( !form.user_id ){
+                form.errors.user_id = 'Debe seleccionar un comercio primero'
+                return
+            }
+            if(!firstStepForm.value.reportValidity()) return
+        }
+
+        if( step.value == 2 && !secondStepForm.value.reportValidity()) return
+        if( step.value == 3){
+            if(form.services.length < 1){
+                $q.notify({message: 'Seleccione por lo menos un servicio', color: 'red'})
+                return
+            }
+            submit()
+        }
+
+        stepper.next()
+    }
 
     const admin = computed(() => {
         if( !form.user_id ) return null;
